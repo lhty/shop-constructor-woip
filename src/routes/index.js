2dfb638f03e5e746c154d33419eb1b4f7a186b9c
lhty <yarik-87@mail.ru>
2019-08-28 20:21:03 +0300

    28-08

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,17 @@ import Header from '../components/header/header';
 import Items from '../components/items/items';
 import Item from '../components/items/item';
 import { BrowserRouter, Route } from 'react-router-dom';
+import VK, { Auth } from 'react-vk';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Header />
+      <div className="centered">
+        <VK apiId={6870521}>
+          <Auth />
+        </VK>
+      </div>
       <Route exact path="/" component={Items} />
       <Route exact path="/:title" component={Item} />
     </BrowserRouter>
