6a538f72d745c87433f6d57eca29c56853bb5fc5
lhty <yarik-87@mail.ru>
2019-08-29 13:14:56 +0300

    test

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './header.css';
 import { Link } from 'react-router-dom';
 import Logo from './logo';
+import VK, { Auth } from 'react-vk';
 
 export default function header() {
   return (
     <div className="header">
       <Link to="/">
         <Logo />
+        <VK apiId={6870521}>
+          <Auth />
+        </VK>
       </Link>
     </div>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,17 +3,12 @@ import Header from '../components/header/header';
 import Items from '../components/items/items';
 import Item from '../components/items/item';
 import { BrowserRouter, Route } from 'react-router-dom';
-import VK, { Auth } from 'react-vk';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Header />
-      <div className="centered">
-        <VK apiId={6870521}>
-          <Auth />
-        </VK>
-      </div>
+      <div className="centered"></div>
       <Route exact path="/" component={Items} />
       <Route exact path="/:title" component={Item} />
     </BrowserRouter>
